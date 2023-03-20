141c4ad0f89195c06467539d5f76dfedbfd31dd8
mlvncnxx <97712598+mlvncnxx@users.noreply.github.com>
2023-03-19 22:38:49 -0400

    initial-commit

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP306_MelVincent_Lab3.Controllers
+{
+    [ApiController]
+    public abstract class BaseController<T> : Controller
+    {
+        protected readonly ILogger<T> _logger;
+        
+        public BaseController(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace COMP306_MelVincent_Lab3.Controllers
+{
+    [Route("health")]
+    public class HealthController : BaseController<HealthController>
+    {
+        public HealthController(ILogger<HealthController> logger) : base(logger)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Health()
+        {
+            return Ok("healthy");
+        }
+    }
+}
